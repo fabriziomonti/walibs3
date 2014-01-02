80472240dac574eaa9edd8ed7f528b39918569c5
fabrizio monti <fabrizio.monti@webappls.com>
2014-01-02 01:15:29 +0100

    - adottata strategia "elettromedicali" x larghezza colonne di default e aggiunto allineamento default x tipo campo

--- watabella/watbl_pdf.php
+++ watabella/watbl_pdf.php
@@ -1,12 +1,4 @@
 <?php
-/**
-* @package waTabella
-* @version 3.0
-* @author G.Gaiba, F.Monti
-* @copyright (c) 2007-2013 {@link http://www.webappls.com WebAppls S.p.A.} Bologna, Italy
-* @license http://www.gnu.org/licenses/gpl.html GPLv3
-*/
-
 include_once(dirname(__FILE__) . '/ya_wam_pdf.php');
 
 //******************************************************************************
@@ -22,9 +14,8 @@ class watbl_pdf extends ya_wam_pdf
 	{
 	var $tabella;
 	
-	var $allineamenti;
-	var $larghezzaPagina;
-	var $larghezzaColonnaDefault;
+	var $larghezze = array();
+	var $allineamenti = array();
 
 	//**************************************************************************
 	/**
@@ -35,24 +26,6 @@ class watbl_pdf extends ya_wam_pdf
 		$this->tabella = $tabella;
 		parent::__construct($this->tabella->pdf_orientazione);
 		$this->AliasNbPages();
-		
-		// calcolo della larghezza percentuale media di una colonna; se non 
-		// viene fornita la larghezza della colonna verra' usato questo valore 
-		// di default
-		$nrCol = 0;
-		foreach ($this->tabella->colonne as $col)
-			{
-			if ($col->mostra)
-				$nrCol++;
-			}
-		
-		$this->larghezzaPagina = $this->w - ($this->lMargin + $this->rMargin);
-		$this->larghezzaColonnaDefault = intval(floor($this->larghezzaPagina / $nrCol));
-		$this->allineamenti = array(WATBL_ALLINEA_CENTRO => 'C', WATBL_ALLINEA_DX => 'R', WATBL_ALLINEA_SX => 'L');
-		
-		$this->Open();
-		$this->SetTitle($table->titolo, true);
-		$this->AddPage();
 		
 		}
 
@@ -70,17 +43,13 @@ class watbl_pdf extends ya_wam_pdf
 			
 		$this->SetFillColor(0xcc);
 		$this->SetFont("Arial",'B', 9);
-		$celle = $larghezze = $allineamenti = array();
+		$celle = array();
 		foreach ($this->tabella->colonne as $col)
 			{
 			if ($col->mostra)
-				{
 				$celle[] = $col->etichetta;
-				$allineamenti[] = $this->allineamenti[$col->allineamento];
-				$larghezze[] = $col->pdf_perc ? intval(floor($this->larghezzaPagina * $col->pdf_perc / 100)) : $this->larghezzaColonnaDefault;
-				}
 			}
-		$this->MultiLineRow($larghezze, 5, $celle, 1, $allineamenti, 1);
+		$this->MultiLineRow($this->larghezze, 5, $celle, 1, $this->allineamenti, 1);
 		
 		}
 	
@@ -94,6 +63,97 @@ class watbl_pdf extends ya_wam_pdf
 		$this->Cell(0, 4, 'pagina ' . $this->PageNo() . " di {nb}", "T", 0, 'C');
 		}
 
+	//**************************************************************************
+	/**
+	 * determina l'allineamento di una colonna secondo quanto definito dal
+	 * programmatore o il default del tipo campo
+	 *
+	 */
+	function dammiAllineamentoColonna(waColonna $col)
+		{
+		static $codici_allineamento = array(WATBL_ALLINEA_CENTRO => 'C', WATBL_ALLINEA_DX => 'R', WATBL_ALLINEA_SX => 'L');
+		if ($col->allineamento)
+			// se è stato definito un allineamento, non c'e' bisogno di 
+			// desumerlo dal tipo campo
+			return $codici_allineamento[$col->allineamento];
+
+		
+		switch ($this->tabella->righeDB->tipoCampo($col->nome))
+			{
+			case WADB_DECIMALE:
+			case WADB_INTERO:
+				return $codici_allineamento[WATBL_ALLINEA_DX];
+				break;
+			case WADB_DATA:
+			case WADB_DATAORA:
+			case WADB_ORA:
+				return $codici_allineamento[WATBL_ALLINEA_CENTRO];
+				break;
+			}
+		
+		return $codici_allineamento[WATBL_ALLINEA_SX];
+		}
+		
+	//**************************************************************************
+	/**
+	 * determina la larghezza di una colonna secondo quanto definito dal
+	 * programmatore o il default del tipo campo
+	 *
+	 */
+	function dammiLarghezzaColonna(waColonna $col, $larghezzaPagina)
+		{
+		$rs = $this->tabella->righeDB;
+
+		if ($col->pdf_perc)
+			return $col->pdf_perc * $larghezzaPagina / 100;
+		elseif ($rs->tipoCampo($col->nome) == WADB_DECIMALE)
+			return 40;
+		elseif ($rs->tipoCampo($col->nome) == WADB_DATA)
+			return 40;
+		elseif ($rs->tipoCampo($col->nome) == WADB_DATAORA)
+			return 40;
+		elseif ($rs->lunghezzaMaxCampo($col->nome) > 50)
+			return 50;
+		elseif ($rs->lunghezzaMaxCampo($col->nome) < 20)
+			return 20;
+
+		return $rs->lunghezzaMaxCampo($col->nome);
+		}
+		
+	//**************************************************************************
+	/**
+	 * determina euristicamente una larghezza delle colonne per 
+	 * cui la percentuale di larghezza non è stata definita e apre il pdf
+	 *
+	 */
+	function apri()
+		{
+		
+		$this->Open($this->tabella->pdf_orientazione);
+		$this->SetTitle($this->tabella->titolo, true);
+		$rs = $this->tabella->righeDB;
+		$larghezzaPagina = $this->w - ($this->lMargin + $this->rMargin);
+		
+		// determinazione degli attributi delle colonne (allineamento e larghezza
+		foreach ($this->tabella->colonne as $col)
+			{
+			if ($col->mostra)
+				{
+				$this->allineamenti[] = $this->dammiAllineamentoColonna($col);
+				$totlen += $this->larghezze[] = $this->dammiLarghezzaColonna ($col, $larghezzaPagina);
+				}
+			}
+			
+		for ($i = 0; $i < count($this->larghezze); $i++)	
+			$totLarghezze += $this->larghezze[$i] = intval(floor($this->larghezze[$i] * $larghezzaPagina / $totlen));
+		
+		// aggiungiamo lo sfrido all'ultima colonna 
+		$this->larghezze[count($this->larghezze) - 1] += intval(floor($larghezzaPagina - $totLarghezze));
+			
+		$this->AddPage();
+		
+		}
+		
 	//**************************************************************************
 	/**
 	 * esporta la tabella
@@ -119,8 +179,6 @@ class watbl_pdf extends ya_wam_pdf
 						if (!empty($col->funzioneCalcolo))
 							$celle[] = call_user_func($col->funzioneCalcolo, $this->tabella);
 						else
-							
-							
 							{
 							switch($col->formattazione)
 								{
@@ -178,20 +236,19 @@ class watbl_pdf extends ya_wam_pdf
 										}
 								}
 							}
-							
-						$allineamenti[] = $this->allineamenti[$col->allineamento];
-						$larghezze[] = $col->pdf_perc ? intval(floor($this->larghezzaPagina * $col->pdf_perc / 100)) : $this->larghezzaColonnaDefault;
 						}
 					}
-				$this->MultiLineRow($larghezze, 4, $celle, 1, $allineamenti, 0);
+				if (!$this->page)
+					$this->apri();
+				
+				$this->MultiLineRow($this->larghezze, 4, $celle, 1, $this->allineamenti, 0);
 				}
 			}
 		
-		$this->Output($this->tabella->soloLettereNumeri($this->tabella->titolo) . date("_YmdHis") . ".pdf", 'D');
+		$this->Output($this->tabella->soloLettereNumeri($this->tabella->titolo) . date("_YmdHis") . ".pdf", 'I');
 		exit();
 		}
 	
 		
 	}
 //*****************************************************************************
-?>
